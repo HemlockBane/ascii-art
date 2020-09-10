81e7b841afe04ccbaf2a91ac20c812876d757a6e
Obinna Igwe <igwenus619@gmail.com>
2020-09-10 14:04:37 +0100

    Add comment on Println

--- dog.go
+++ dog.go
@@ -1,11 +1,18 @@
 package main
 
 import f "fmt"
+import "time"
 
 func main(){
   f.Println("  __      _")
   f.Println("o'')}____//")
   f.Println(" `_/      )")
   f.Println(" (_(_/-(_/")
+
+
+  f.Printf("Printed at %s", time.Now().String());
+
+  // Interesting fact: PrintLn doesn't support format specifiers
+  // Here's a link I found: https://stackoverflow.com/questions/45645900/fmt-println-prints-out-format-verbs-like-s
 
 }
